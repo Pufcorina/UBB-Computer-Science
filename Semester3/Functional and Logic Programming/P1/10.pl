7c6543cbaa6278f49ab0ad5719ae066b1e4052c1
Corina Todoran <corina_todoran_ana@yahoo.com>
2017-12-26 01:42:23 +0200

    P1 -> 10

---
+++ Semester3/Functional and Logic Programming/P1/10.pl
@@ -0,0 +1,40 @@
+% 10.
+% a. Define a predicate to test if a list of an integer elements has a "valley" aspect (a set has a "valley" aspect if
+% elements decreases up to a certain point, and then increases.
+% eg: 10 8 6 9 11 13 – has a “valley” aspect
+% b. Calculate the alternate sum of list’s elements (l1 - l2 + l3 ...).
+
+% a
+
+% Model matematic:
+% valey(l1...ln, f) =
+% 	true, n = 1 and f = 0
+% 	valey(l2...ln, 0), l1 < l2
+% 	valey(l2...ln, 1), l1 > l2
+% 	false, otherwise
+
+% valey(L:list, F:number)
+% valey(i, i)
+
+valey([_], 0).
+valey([H1, H2|T], _) :- H1 < H2, 
+    valey([H2|T], 0), !.
+valey([H1, H2|T], 1) :- H1 > H2,
+    valey([H2|T], 1), !.
+
+% b
+
+% Model matematic:
+% alternative_sum(l1...ln) =
+% 	0, n = 0
+% 	l1, n = 1
+% 	l1 - l2 + alternative_sum(l3...ln), otherwise
+
+% alternative_sum(L:list, R:number)
+% alternative_sum(i, o)
+
+alternative_sum([], 0).
+alternative_sum([H], H).
+alternative_sum([H1, H2|T], R) :-
+    alternative_sum(T, R1),
+    R is H1 - H2 + R1.
